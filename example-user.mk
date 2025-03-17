1894350011705f7b79ec3c815023cf739016a9c0
Derald D. Woods <woods.technical@gmail.com>
2025-03-16 20:52:27 -0400

    example-user.mk: improve expected content

--- example-user.mk
+++ example-user.mk
@@ -1,3 +1,12 @@
-CC="/path/to/toolchain/bin/$(CROSS_COMPILE)gcc"
-CFLAGS_USER="-isysroot /path/to/toolchain/sysroot"
-LDFLAGS_USER="--sysroot=/path/to/toolchain/sysroot"
+CROSS_PREFIX := <arch>-<vendor>-<os>-<eabi>
+CROSS_COMPILE := $(CROSS_PREFIX)-
+TOOLCHAIN_DIR := /path/to/toolchain/$(CROSS_PREFIX)
+STAGING_DIR := /path/to/staging
+
+export PATH := $(TOOLCHAIN_DIR)/bin:$(PATH)
+
+CC := $(CROSS_COMPILE)gcc
+CFLAGS_USER := -isysroot $(shell $(TOOLCHAIN_DIR)/bin/$(CROSS_COMPILE)gcc -print-sysroot) \
+	       -I$(STAGING_DIR)/usr/include
+LDFLAGS_USER := --sysroot=$(shell $(TOOLCHAIN_DIR)/bin/$(CROSS_COMPILE)gcc -print-sysroot) \
+	       -L$(STAGING_DIR)/usr/lib
